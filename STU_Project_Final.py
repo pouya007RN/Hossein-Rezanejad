1ae34b498a005ff3ad19b041cdf961be18b1c768
pouya007RN <35175878+pouya007RN@users.noreply.github.com>
2018-04-22 09:47:42 -0700

    Add files via upload

---
+++ STU_Project_Final.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+import time,pytesseract
+from PIL import Image
+
+User = '96521236'     #Username and password
+Pass = '4830137614'
+
+driver_path = r"C:\Users\CHS\Desktop\MicrosoftWebDriver.exe"
+driver = webdriver.Edge(driver_path)                  #location of chrome webdriver
+
+driver.get("http://stu.iust.ac.ir/loginpage.rose")  # the determined URL
+
+elem = driver.find_element_by_id('j_username')
+elem.send_keys(User)
+
+time.sleep(1)                                   #applying Username and password
+
+elem = driver.find_element_by_id('j_password')
+elem.send_keys(Pass)
+
+IM_OBJ = driver.find_element_by_id('captcha_img')
+Saved_IM = IM_OBJ.screenshot('captcha.png')
+captcha = Image.open('captcha.png','r')
+captcha_string = pytesseract.image_to_string(captcha, lang='eng')
+
+elem = driver.find_element_by_id('captcha_input')
+elem.click()
+elem.send_keys(captcha_string)
+
+time.sleep(3) #time required to fill the Captcha Input Field
+
+driver.find_element_by_xpath('//*[@id="login_btn_submit"]').click()  #logging in
+
+time.sleep(8)
+
+driver.find_element_by_xpath('//*[@id="1_div"]/div[1]/a/img').click()
+
+time.sleep(8)
+
+A = driver.find_element_by_id('nextWeekBtn')
+A.click()
+
+time.sleep(8)
+
+'''Food_P = ['شنيسل مرغ','تن ماهي','کوبيده','قيمه']
+Food_P_by_id = ['userWeekReserve.selected3','userWeekReserve.selected5','userWeekReserve.selected7']
+
+
+
+for Food in range(3):
+    driver.find_element_by_id(Food_P_by_id[Food]).click()
+
+    time.sleep(4)'''
+
+                              
+
+driver.find_element_by_xpath('//*[@id="userWeekReserves.selected1"]').click()
+
+time.sleep(2)                                                                     #selecting food for two days
+
+driver.find_element_by_xpath('//*[@id="userWeekReserves.selected3"]').click()
+
+time.sleep(2)
+
+driver.find_element_by_xpath('//*[@id="userWeekReserves.selected5"]').click()
+
+time.sleep(2)
+
+driver.find_element_by_xpath('//*[@id="userWeekReserves.selected6"]').click()
+
+time.sleep(3)
+
+driver.find_element_by_xpath('//*[@id="doReservBtn"]').click()       #Reservation
+
+time.sleep(5)
+
+
+driver.quit()        #DONE:D
+
